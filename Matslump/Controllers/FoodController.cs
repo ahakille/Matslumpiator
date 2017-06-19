344799e1f021d7f1b97598d4aab00d320277f0a0
Nicklas Persson <gorlingy@hotmail.com>
2017-06-19 23:48:09 +0200

    Fixat page

--- Matslump/Controllers/FoodController.cs
+++ Matslump/Controllers/FoodController.cs
@@ -1,6 +1,7 @@
 ﻿using Matslump.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace Matslump.Controllers
@@ -8,15 +9,21 @@ namespace Matslump.Controllers
     public class FoodController : Controller
     {
         // GET: Food
-        public ActionResult All()
+        public ActionResult All(int? page)
         {
-            List<Receptmodels> food_list = new List<Receptmodels>();
             Receptmodels re = new Receptmodels();
             re.Recept = re.getFood("SELECT * FROM recept ",Convert.ToInt32(User.Identity.Name));
             ViewBag.Myfood= re.getFood("SELECT * FROM recept WHERE id_recept IN (SELECT recept_id FROM users_has_recept WHERE user_id =@id_user)", Convert.ToInt32(User.Identity.Name));
-
+            var recept = re.Recept;
             ViewBag.food = re.Recept;
-            return View(re);
+            var pager = new Pager(re.Recept.Count, page);
+
+            var viewModel = new IndexViewModel
+            {
+                Items = recept.Skip((pager.CurrentPage - 1) * pager.PageSize).Take(pager.PageSize),
+                Pager = pager
+            };
+            return View(viewModel);
         }
         public ActionResult AddNewFood()
         {
@@ -65,11 +72,11 @@ namespace Matslump.Controllers
             return RedirectToAction("ALL", "Food");
         }
 
-        public ActionResult AddToMyFood(int id)
+        public ActionResult AddToMyFood(int id ,int page)
         {
             Receptmodels re = new Receptmodels();
             re.addFood_user(User.Identity.Name, id);
-            return RedirectToAction("ALL", "Food");
+            return RedirectToAction("ALL", "Food",new { page = page });
 
         }
     }
--- Matslump/Controllers/MyfoodController.cs
+++ Matslump/Controllers/MyfoodController.cs
@@ -1,6 +1,7 @@
 ﻿using Matslump.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace Matslump.Controllers
@@ -8,13 +9,21 @@ namespace Matslump.Controllers
     public class MyfoodController : Controller
     {
         // GET: Myfood
-        public ActionResult Index()
+        public ActionResult Index(int? page)
         {
             List<Receptmodels> food_list = new List<Receptmodels>();
             Receptmodels re = new Receptmodels();
             re.Recept = re.getFood("SELECT * FROM recept WHERE id_recept IN (SELECT recept_id FROM users_has_recept WHERE user_id =@id_user)", Convert.ToInt32( User.Identity.Name));
-            ViewBag.food = food_list;
-            return View(re);
+            var recept = re.Recept;
+            var pager = new Pager(re.Recept.Count, page);
+
+            var viewModel = new IndexViewModel
+            {
+                Items = recept.Skip((pager.CurrentPage - 1) * pager.PageSize).Take(pager.PageSize),
+                Pager = pager
+            };
+            return View(viewModel);
+            
         }
 
         // GET: Myfood/Details/5
@@ -32,11 +41,11 @@ namespace Matslump.Controllers
         }
 
         // GET: Myfood/Delete/5
-        public ActionResult Delete(int id)
+        public ActionResult Delete(int id, int page)
         {
             Receptmodels re = new Receptmodels();
             re.removefood_user(User.Identity.Name, id);
-            return RedirectToAction("index");
+            return RedirectToAction("index",new { page = page });
         }
 
         
