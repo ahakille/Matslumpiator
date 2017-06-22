293212e281b08b96c8cce4d9e9d352a82d294c09
Nicklas Persson <gorlingy@hotmail.com>
2017-06-22 22:34:55 +0200

    Cron

---
+++ cron/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cron
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
