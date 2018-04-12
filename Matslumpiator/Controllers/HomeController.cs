b12e474ddb04095d6afd3c4a9632a87b667220d5
Nicklas Persson <gorlingy@hotmail.com>
2018-04-13 00:45:04 +0200

    Buggfix useragg

--- Matslumpiator/Controllers/HomeController.cs
+++ Matslumpiator/Controllers/HomeController.cs
@@ -21,19 +21,13 @@ namespace Matslumpiator.Controllers
             return View();
         }
         
-        public IActionResult About()
+        public IActionResult Useragreement()
         {
-            ViewData["Message"] = "Your application description page.";
+            
 
             return View();
         }
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
 
         public IActionResult Error()
         {
